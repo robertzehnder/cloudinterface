cb9fc545be5ed1fb85657175fab3bfa5fbfd4e49
Robert Zehnder <robzcodes@gmail.com>
2017-12-18 19:23:39 -0500

    initial commit after yeoman generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cloudinterfaceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cloudinterfaceApp
+ */
+angular.module('cloudinterfaceApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
